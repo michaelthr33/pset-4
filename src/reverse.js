73e39727a25b9d2dd659c9fec5dc03909eb1b51a
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-11-18 09:30:12 -0500

    sum finished, reverse started

--- src/reverse.js
+++ src/reverse.js
@@ -1,9 +1,16 @@
 var readlineSync = require('readline-sync');
 const MAX= Number.MAX_SAFE_INTEGER
 const MIN= Number.MIN_SAFE_INTEGER
-let lowerbound = Number(readlineSync.question("Lower bound: "));
-let upperbound = Number(readlineSync.question("Upper bound: "));
-if (upperbound>MAX||lowerbound>MAX||upperbound<MIN||lowerbound<MIN){console.log"Invalid"}
-else if (lowerbound>=upperbound) {let lowerbound = Number(readlineSync.question("Lower bound :"));
-let upperbound = Number(readlineSync.question("Upper bound: "));
-}
+let positive="hello"
+do {
+  positive=(readlineSync.question("Positive Integer: "))
+} while(positive>=MAX||positive<=0)
+
+let length=positive.length;
+positive=Number(positive);
+length=lenght-1
+
+let ceiling=(Math.pow(length,10))
+let firstvalue=Math.ceil(positive);
+positive=positive-firstvalue
+let ceiling=
--- src/sum.js
+++ src/sum.js
@@ -1,9 +1,21 @@
 var readlineSync = require('readline-sync');
 const MAX= Number.MAX_SAFE_INTEGER
 const MIN= Number.MIN_SAFE_INTEGER
-let lowerbound = Number(readlineSync.question("Lower bound: "));
-let upperbound = Number(readlineSync.question("Upper bound: "));
-if (upperbound>MAX||lowerbound>MAX||upperbound<MIN||lowerbound<MIN){console.log"Invalid"}
-else if (lowerbound>=upperbound) {let lowerbound = Number(readlineSync.question("Lower bound :"));
-let upperbound = Number(readlineSync.question("Upper bound: "));
-}
+let lowerbound=0
+let upperbound=1
+
+do {
+ lowerbound = Number(readlineSync.question("Lower bound: "));
+ upperbound = Number(readlineSync.question("Upper bound: "));
+} while (lowerbound >= upperbound)
+
+
+if (upperbound>MAX||lowerbound>MAX||upperbound<MIN||lowerbound<MIN){console.log("Invalid")}
+
+let sum = 0;
+let counter = sum * 2
+while(lowerbound<=upperbound){
+  if(lowerbound%2>=0&&lowerbound%2<=0){sum=sum+lowerbound;
+  }lowerbound++;}
+
+console.log(''+sum+'.')
