02d69a2afa11730ba96da3b3e8dfcf0d882841b0
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-11-22 10:20:58 -0500

    prime.js completed

--- src/prime.js
+++ src/prime.js
@@ -1,15 +1,21 @@
 var readlineSync = require('readline-sync');
 const MAX= Number.MAX_SAFE_INTEGER
 const MIN= Number.MIN_SAFE_INTEGER
-let positive=0
-let sum=0
-let counter=0
- do{
-   positive =Number(readlineSync.question("Non-negative integer: "))
-   if(positive>=0&positive<=MAX){
-     sum=sum+positive;
-     counter=counter+1;
- }
-} while(positive>=0&positive<MAX);
-let average= sum/counter
-console.log(average)
+let positive =0
+let prime=0
+let divide=2
+do
+{
+  positive=Number(readlineSync.question("Non-negative integer: "))
+  if (positive>=0&positive<=MAX)
+  {
+    while(divide<positive&prime==0)
+    {
+      if(positive%divide==0){prime=1}
+      divide=divide+1
+    }
+  }
+} while (positive<0||positive>MAX)
+
+if(prime==1){console.log("Not prime.")}
+if(prime==0){console.log("Prime.")}
